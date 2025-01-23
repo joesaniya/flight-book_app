3476f20c5467425eaddd854d6198537298e48ea8
Esther Jenslin <esther@katenterprise.com>
2025-01-24 00:18:10 +0530

    added

--- lib/view/widgets/insprationwidget.dart
+++ lib/view/widgets/insprationwidget.dart
@@ -14,64 +14,61 @@ class InspirationListWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetBuilder<HomeController>(builder: (controller) {
       return Container(
+        margin: const EdgeInsets.symmetric(horizontal: 20.0),
         child: Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: Row(
-                children: [
-                  Text(
-                    'Travel Inspirations',
-                    style: GoogleFonts.metrophobic(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600,
-                      height: 16 / 16,
-                      decoration: TextDecoration.none,
-                      textBaseline: TextBaseline.alphabetic,
-                    ),
-                    textAlign: TextAlign.left,
+            Row(
+              children: [
+                Text(
+                  'Travel Inspirations',
+                  style: GoogleFonts.metrophobic(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    height: 16 / 16,
+                    decoration: TextDecoration.none,
+                    textBaseline: TextBaseline.alphabetic,
                   ),
-                  Expanded(child: Container()),
-                  Expanded(
-                    child: Container(
-                      child: DropdownButton<String>(
-                        value: controller.selectedDestination,
-                        onChanged: (String? newValue) {
-                          controller.setSelectedDestination(newValue!);
-                        },
-                        items: controller.destination.map((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(
-                              value,
-                              style: GoogleFonts.metrophobic(
-                                fontSize: 16,
-                                fontWeight: FontWeight.w500,
-                                height: 1.0,
-                                letterSpacing: 0.1,
-                              ),
+                  textAlign: TextAlign.left,
+                ),
+                Expanded(child: Container()),
+                Expanded(
+                  child: Container(
+                    child: DropdownButton<String>(
+                      value: controller.selectedDestination,
+                      onChanged: (String? newValue) {
+                        controller.setSelectedDestination(newValue!);
+                      },
+                      items: controller.destination.map((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(
+                            value,
+                            style: GoogleFonts.metrophobic(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                              height: 1.0,
+                              letterSpacing: 0.1,
                             ),
-                          );
-                        }).toList(),
-                        underline: Container(
-                          height: 1,
-                          color: Colors.black,
-                        ),
-                        icon: const Icon(
-                          Icons.arrow_drop_down,
-                          color: Colors.green,
-                        ),
-                        isExpanded: true,
+                          ),
+                        );
+                      }).toList(),
+                      underline: Container(
+                        height: 1,
+                        color: Colors.black,
                       ),
+                      icon: const Icon(
+                        Icons.arrow_drop_down,
+                        color: Colors.green,
+                      ),
+                      isExpanded: true,
                     ),
-                  )
-                ],
-              ),
+                  ),
+                )
+              ],
             ),
             SizedBox(
               height: MediaQuery.of(context).size.height * 0.28,
               child: ListView.separated(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
                 scrollDirection: Axis.horizontal,
                 itemCount: controller.inspirationList.length,
                 separatorBuilder: (context, index) => const SizedBox(width: 10),
